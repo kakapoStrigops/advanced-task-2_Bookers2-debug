29757cf31464064d7a06b57e98bfcb7f6507ff04
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-07-30 13:34:29 +0000

    コメント投稿機能のコントローラの修正(book_comments_controller.rbのcreateアクション)

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,9 +1,9 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:post_image_id])
-    comment = current_user.books.new(book_comment_params)
-    comment.book_id = book_image.id
+    book = Book.find(params[:book_id])
+    comment = current_user.book_comments.new(book_comment_params)
+    comment.book_id = book.id
     comment.save
     redirect_to book_path(book)
   end
