7a527be44c076314f040215f571da2ae6ac90e3e
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-08-07 00:36:01 +0000

    コメント削除機能の非同期通信化の途中

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -9,7 +9,9 @@ class BookCommentsController < ApplicationController
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    @book_comment.destroy
     # redirect_to book_path(params[:book_id])
   end
 
