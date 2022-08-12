54eb666b0d30d00cb16ee63b7c55e175a2d85e00
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-08-12 03:14:15 +0000

    修正作業時時に生じた余計な記載を削除

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
   before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
-  def shows
+  def show
     @book = Book.find(params[:id])
     @book_new = Book.new
     @book_comment = BookComment.new
